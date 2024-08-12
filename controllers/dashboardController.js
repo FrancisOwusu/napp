2f0b6335bcac9f6b2b870e3fd466ec64ff3db8da
fowusu <osei1257@gmail.com>
2024-08-12 17:24:16 +0000

    fix to ticket in dashboard controller

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,34 +1,40 @@
+const dashboardService = require('../services/dashboardService')
+const  Ticket = require('../database/models')
+const models = require('../database/models/index');
+const Sequelize=require('sequelize')
+// const Ticket = models.
 module.exports = {
     index:async(req,res)=>{
   
         try {
-            const totalTickets = await Ticket.count(); // Total number of tickets
+            const totalTickets = await models.Ticket.count();
+            // Ticket.count(); // Total number of tickets
     
-            const assignedTickets = await Ticket.count({
+            const assignedTickets = await models.Ticket.count({
               where: {
                 assignee_id: { [Sequelize.Op.ne]: null },
               },
             }); // Tickets that are assigned
         
-            const unassignedTickets = await Ticket.count({
+            const unassignedTickets = await models.Ticket.count({
               where: {
                 assignee_id: null,
               },
             }); // Tickets that are unassigned
         
-            const newTickets = await Ticket.count({
+            const newTickets = await models.Ticket.count({
               where: {
                 status: 'new',
               },
             }); // Tickets that are new
         
-            const inProgressTickets = await Ticket.count({
+            const inProgressTickets = await models.Ticket.count({
               where: {
                 status: 'in_progress',
               },
             }); // Tickets that are in progress
         
-            const completedTickets = await Ticket.count({
+            const completedTickets = await models.Ticket.count({
               where: {
                 status: 'completed',
               },
