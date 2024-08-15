e5e5b8f05ff788f154d68c0c236e7581c74ad28e
fowusu <osei1257@gmail.com>
2024-08-15 07:39:20 +0000

    add ticketController and route

---
+++ controllers/ticketStatusController.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const baseController = require("./baseController");
+const {TicketStatusService} = require("../services");
+module.exports = {
+  ...baseController(TicketStatusService),
+}
--- services/index.js
+++ services/index.js
@@ -1,18 +1,17 @@
-const { TicketFileRepository } = require('../repository');
-
-module.exports ={
-    // AuthService:require('./authService'),
-    UserService:require('./userService'),
-    EmailService:require('./emailService'),
-    RoleService:require('./roleService'),
-    CategoryService:require('./categoryService'),
-    PermissionService: require('./permissionService'),
-    RolePermissionService:require('./rolePermissionService'),
-    RoleUserService:require('./roleUserService'),
-    UserPermissionService: require('./userPermissionService'),
-    PriorityService:require('./priorityService'),
-    TicketService: require('./ticketService'),
-    TicketFileService:require('./ticketFileService'),
-    DashboardService:require('./dashboardService'),
-    TicketStatusRepository:require('./ticketStatusService')
-}
+"use strict";
+module.exports = {
+  // AuthService:require('./authService'),
+  UserService: require("./userService"),
+  EmailService: require("./emailService"),
+  RoleService: require("./roleService"),
+  CategoryService: require("./categoryService"),
+  PermissionService: require("./permissionService"),
+  RolePermissionService: require("./rolePermissionService"),
+  RoleUserService: require("./roleUserService"),
+  UserPermissionService: require("./userPermissionService"),
+  PriorityService: require("./priorityService"),
+  TicketService: require("./ticketService"),
+  TicketFileService: require("./ticketFileService"),
+  DashboardService: require("./dashboardService"),
+  TicketStatusRepository: require("./ticketStatusService"),
+};
