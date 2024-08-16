572e81ba1346506312510d9e2d6946c5e94b1bcb
fowusu <osei1257@gmail.com>
2024-08-16 00:57:21 +0000

    change dashboard route in routes

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ module.exports = (app) => {
   app.use("/userpermissions", require("./userPermissionRoute"));
   app.use("/priorities", require("./priorityRoute"));
   app.use("/tickets", authenticate, require("./ticketRoute"));
-  app.use("/dashboard", authenticate, require("./dashboardRoute"));
+  app.use("/dashboard", require("./dashboardRoute"));
   app.use("/statuses", require("./ticketStatusRoute"));
   app.use("*", (_req, res) =>
     res.status(404).json({ message: "route not found" })
